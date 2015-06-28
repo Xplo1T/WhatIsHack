55feeca733361306ce91c0c13a6678fae7b5f6a7
newchild <fabianbrand98@gmx.de>
2015-06-28 22:56:08 +0200

    jQuery, I will yolooooooooooooooooo

---
+++ What is Hack/What is Hack/LeagueAdressesOffsets.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLAPIInfo
+{
+	static class LeagueAdressesOffsets
+	{
+
+		public static class PlayerObject
+		{
+			public static int BaseAdress = 0x024EE850;
+			public static class Attributes
+			{
+				public static int Offset = 0xFC;
+				public static class Gold
+				{
+					public static int Offset = 0xC;
+				}
+				public static class MoveSpeed
+				{
+					public static int Offset = 0x30;
+				}
+				public static class AD
+				{
+					public static int Offset = 0x24;
+				}
+			}
+			public static class Stats
+			{
+				public static int Offset = 0x100;
+				
+				public static class currentHealth
+				{
+					public static int Offset = 0x3C;
+				}
+				public static class MaxHealth
+				{
+					public static int Offset = 0x40;
+				}
+				public static class currentMana
+				{
+					public static int Offset = 0x4C;
+				}
+				public static class MaxMana
+				{
+					public static int Offset = 0x50;
+				}
+			}
+		}
+
+	}
+}
---
+++ What is Hack/What is Hack/SimpleLoL.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleMemory;
+using System.Threading.Tasks;
+using LoLAPIInfo;
+
+namespace LoLAPI
+{
+		public class Player
+		{
+
+		private int getUserGold()
+			{
+				MemoryManager x = new MemoryManager();
+				x.Attach("League of Legends");
+				var GoldLv1Adress = (IntPtr)(x.ReadInteger(LeagueAdressesOffsets.PlayerObject.BaseAdress) + LeagueAdressesOffsets.PlayerObject.Attributes.Offset);
+				var GoldAdress = (IntPtr)(x.ReadInteger(GoldLv1Adress.ToInt32()) + LeagueAdressesOffsets.PlayerObject.Attributes.Gold.Offset);
+				var gold = x.ReadInteger(GoldAdress.ToInt32());
+				x.Disconnect();
+				return gold;
+			}
+			private int getCurrentMana()
+			{
+				MemoryManager x = new MemoryManager();
+				x.Attach("League of Legends");
+				var GoldLv1Adress = (IntPtr)(x.ReadInteger(LeagueAdressesOffsets.PlayerObject.BaseAdress) + LeagueAdressesOffsets.PlayerObject.Stats.Offset);
+				var GoldAdress = (IntPtr)(x.ReadInteger(GoldLv1Adress.ToInt32()) + LeagueAdressesOffsets.PlayerObject.Stats.currentMana.Offset);
+				var gold = x.ReadInteger(GoldAdress.ToInt32());
+				x.Disconnect();
+				return gold;
+			}
+			private int getMaximumMana()
+			{
+				MemoryManager x = new MemoryManager();
+				x.Attach("League of Legends");
+				var GoldLv1Adress = (IntPtr)(x.ReadInteger(LeagueAdressesOffsets.PlayerObject.BaseAdress) + LeagueAdressesOffsets.PlayerObject.Stats.Offset);
+				var GoldAdress = (IntPtr)(x.ReadInteger(GoldLv1Adress.ToInt32()) + LeagueAdressesOffsets.PlayerObject.Stats.MaxMana.Offset);
+				var gold = x.ReadInteger(GoldAdress.ToInt32());
+				x.Disconnect();
+				return gold;
+			}
+			private int getMaximumHealth()
+			{
+				MemoryManager x = new MemoryManager();
+				x.Attach("League of Legends");
+				var GoldLv1Adress = (IntPtr)(x.ReadInteger(LeagueAdressesOffsets.PlayerObject.BaseAdress) + LeagueAdressesOffsets.PlayerObject.Stats.Offset);
+				var GoldAdress = (IntPtr)(x.ReadInteger(GoldLv1Adress.ToInt32()) + LeagueAdressesOffsets.PlayerObject.Stats.MaxHealth.Offset);
+				var gold = x.ReadInteger(GoldAdress.ToInt32());
+				x.Disconnect();
+				return gold;
+			}
+			private int getCurrentHealth()
+			{
+				MemoryManager x = new MemoryManager();
+				x.Attach("League of Legends");
+				var GoldLv1Adress = (IntPtr)(x.ReadInteger(LeagueAdressesOffsets.PlayerObject.BaseAdress) + LeagueAdressesOffsets.PlayerObject.Stats.Offset);
+				var GoldAdress = (IntPtr)(x.ReadInteger(GoldLv1Adress.ToInt32()) + LeagueAdressesOffsets.PlayerObject.Stats.currentHealth.Offset);
+				var gold = x.ReadInteger(GoldAdress.ToInt32());
+				x.Disconnect();
+				return gold;
+			}
+			private int getAD()
+			{
+			MemoryManager x = new MemoryManager();
+			x.Attach("League of Legends");
+			var GoldLv1Adress = (IntPtr)(x.ReadInteger(LeagueAdressesOffsets.PlayerObject.BaseAdress) + LeagueAdressesOffsets.PlayerObject.Attributes.Offset);
+			var GoldAdress = (IntPtr)(x.ReadInteger(GoldLv1Adress.ToInt32()) + LeagueAdressesOffsets.PlayerObject.Attributes.AD.Offset);
+			var gold = x.ReadFloat(GoldAdress.ToInt32());
+			x.Disconnect();
+			return Convert.ToInt32(Math.Round(gold));
+			}
+		public int CurrentMana{
+				get {
+					return getCurrentMana();
+				}
+			}
+
+			public int MaximumMana
+			{
+				get
+				{
+					return getMaximumMana();
+				}
+			}
+			public int AD
+			{
+				get
+				{
+					return getAD();
+				}
+			}
+		public int MaximumHealth
+			{
+				get
+				{
+					return getMaximumHealth();
+				}
+			}
+			public int CurrentHealth
+			{
+				get
+				{
+					return getCurrentHealth();
+				}
+			}
+			public int Gold
+			{
+				get
+				{
+					return getUserGold();
+				}
+			}
+		}
+		
+	
+}
